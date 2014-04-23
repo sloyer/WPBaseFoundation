d4c1bc45ad9def99705cafb89a3c96dbfa0a7a88
Steven Loyer <reyoldesign@gmail.com>
2014-04-23 11:52:27 -0800

    Cleaning up old markup from previous site that got carried over

--- entry.php
+++ entry.php
@@ -1,26 +1,19 @@
-<div class="wrap3">
-    <div class="row">
-        <div class="front-content">
-            <h2 class="title">Blogs</h2>
-        <?php
-             $postslist = get_posts('numberposts=3&order=DESC&orderby=date');
-             foreach ($postslist as $post) :
-                setup_postdata($post);
-             ?>
-             <div class="entry">
-                 <span class="post-thumbnail"><?php the_post_thumbnail('thumbnail');?></span>
-                 <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                 <h3 class="post-date"> <?php the_time(get_option('date_format')) ?></h3>
-                 <?php the_excerpt(); ?>
-            </div>
-                <?php endforeach; ?>
-                <?php wp_reset_postdata(); ?>
-            </div>
-        <div class="upcoming">
-            <?php dynamic_sidebar( 'sidebar1' ); ?>
-        </div>
-        <div class="testimonials">
-
-        </div>
+<div class="row">
+  <div class="front-content">
+    <h2 class="title">Entrys</h2>
+    <?php
+     $postslist = get_posts('numberposts=3&order=DESC&orderby=date');
+     foreach ($postslist as $post) :
+	setup_postdata($post);
+     ?>
+     <div class="entry">
+	 <span class="post-thumbnail"><?php the_post_thumbnail('thumbnail');?></span>
+	 <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+	 <h3 class="post-date"> <?php the_time(get_option('date_format')) ?></h3>
+	 <?php the_excerpt(); ?>
     </div>
+	<?php endforeach; ?>
+	<?php wp_reset_postdata(); ?>
+  </div>
 </div>
+
